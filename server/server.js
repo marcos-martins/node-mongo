9d625f452d3eb3075cdf3f579bf4a71ae9bc3e6f
marcos-martins <m.martins@outlook.com>
2018-01-03 20:11:10 -0200

    Sepetated db and init node express

--- server/server.js
+++ server/server.js
@@ -1,28 +1,3 @@
-var mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
-var Todo = mongoose.model('Todo',{
-  text:{
-    type: String
-  },
-  completed:{
-    type: Boolean
-  },
-  completedAt:{
-    type: Number
-  }
-});
-
-var newTodo = new Todo({
-  text:'Study Math',
-  completed: true,
-  completedAt: 5
-});
-
-newTodo.save().then((doc)=>{
-  console.log('Saved todo', doc);
-}, (e)=>{
-  console.log('Unable to save todo.');
-});
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
