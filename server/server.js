3a9f2d861c059eec487c57782e8359c5b37a257f
marcos-martins <m.martins@outlook.com>
2018-01-03 22:16:41 -0200

    GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -32,6 +32,21 @@ app.get('/todos', (req, res) =>{
   });
 });
 
+app.get('/todos/:id', (req, res) =>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send(todo);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
 
 app.listen(port, ()=>{
   console.log('Start');
