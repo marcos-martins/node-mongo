d02340e3823babffc8118e6a53771423071c3fdd
marcos-martins <m.martins@outlook.com>
2018-01-03 22:57:59 -0200

    Alter method post todo

--- server/server.js
+++ server/server.js
@@ -13,7 +13,9 @@ app.use(bodyParser.json());
 
 app.post('/todos',(req, res) => {
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    completed: req.body.completed,
+    completedAt: req.body.completedAt
   });
 
   todo.save().then((doc) =>{
