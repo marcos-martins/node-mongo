3237015c9fcc8b3b38eaf5ea35c7a039ca27d1c3
marcos-martins <m.martins@outlook.com>
2018-01-07 14:31:48 -0200

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,15 @@ UserSchema.methods.generateAuthToken = function(){
   });
 };
 
+UserSchema.methods.removeToken = function(token){
+  var user = this;
+  return user.update({
+    $pull:{
+      tokens:{token}
+    }
+  });
+};
+
 UserSchema.statics.findByToken = function (token){
   var User = this;
   var decoded;
